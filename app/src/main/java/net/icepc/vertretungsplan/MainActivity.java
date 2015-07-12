34a4abd4af6d2a0bc3c47aab1f8780751d33d574
Patrice Becker <patrice@5becker.de>
2015-07-12 21:22:18 +0200

    Swipable Tabs

--- app/src/main/java/net/icepc/vertretungsplan/MainActivity.java
+++ app/src/main/java/net/icepc/vertretungsplan/MainActivity.java
@@ -1,17 +1,12 @@
 package net.icepc.vertretungsplan;
 
-import android.support.v4.view.PagerAdapter;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-
-import it.neokree.materialtabs.MaterialTab;
-import it.neokree.materialtabs.MaterialTabHost;
-import it.neokree.materialtabs.MaterialTabListener;
 
 
-public class MainActivity extends ActionBarActivity implements MaterialTabListener{
-    MaterialTabHost tabHost;
+public class MainActivity extends ActionBarActivity {
+    //MaterialTabHost tabHost;
     ViewPager pager;
     ViewPagerAdapter adapter;
 
@@ -21,44 +16,30 @@ public class MainActivity extends ActionBarActivity implements MaterialTabListen
         setContentView(R.layout.activity_main);
         getSupportActionBar().setElevation(0);
 
+        /*
         tabHost = (MaterialTabHost) this.findViewById(R.id.materialTabHost);
+        tabHost.setAnimation(null);
+        */
+
+        SlidingTabLayout tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true);
+
         pager = (ViewPager) this.findViewById(R.id.viewpager);
 
         // init view pager
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
         pager.setAdapter(adapter);
-        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
-            public void onPageSelected(int position) {
-                // when user do a swipe the selected tab change
-                tabHost.setSelectedNavigationItem(position);
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.accentColor);
             }
         });
+        tabs.setViewPager(pager);
 
-        // insert all tabs from pagerAdapter data
-        for (int i = 0; i < adapter.getCount(); i++) {
-            tabHost.addTab(
-                    tabHost.newTab()
-                            .setText(adapter.getPageTitle(i))
-                            .setTabListener(this)
-            );
-        }
-    }
-
-    @Override
-    public void onTabSelected(MaterialTab materialTab) {
-        pager.setCurrentItem(materialTab.getPosition());
 
     }
 
-    @Override
-    public void onTabReselected(MaterialTab materialTab) {
-
-    }
-
-    @Override
-    public void onTabUnselected(MaterialTab materialTab) {
-
-    }
 }
